92e250e6f2e17e1ed8cc2499f10d3ffec91bc138
Aguda <agudaoluwanifemi@gmail.com>
2022-05-30 12:03:32 +0100

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// import Body1 from './components/Body1';
-// import Navigation from './components/Navigation';
-// import Body3 from './components/Body3';
-// import Body4 from './components/Body4';
 import Body5 from './components/Body5';
 import './index.css';
 
